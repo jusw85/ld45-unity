dfbc760805ae47295d5db7140711b85a71b1b270
ziczach <czc85@hotmail.com>
2019-10-05 23:07:25 +0800

    Updated Level 1 tiled map

---
+++ Assets/Project/Tilemaps/Environment_Sprite_Sheet_64.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="Environment_Sprite_Sheet_64" tilewidth="64" tileheight="64" tilecount="128" columns="16">
+ <image source="spritesheets/Environment_Sprite_Sheet_64.png" width="1024" height="512"/>
+</tileset>
