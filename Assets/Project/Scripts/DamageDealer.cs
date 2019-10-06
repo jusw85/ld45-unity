42e52fa971058228967ac4224b99ad23333129ec
Justin Wong <jusw85@hotmail.com>
2019-10-06 15:15:25 +0800

    Add unit health and health bar

---
+++ Assets/Project/Scripts/DamageDealer.cs
@@ -0,0 +1,23 @@
+﻿using ScriptableObjectArchitecture;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+    [SerializeField] private FloatReference damageAmount = default(FloatReference);
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        UnitHealth targetHealth = other.gameObject.GetComponent<UnitHealth>();
+
+        if (targetHealth != null)
+        {
+            targetHealth.Damage(damageAmount.Value);
+        }
+    }
+
+    public FloatReference DamageAmount
+    {
+        get { return damageAmount; }
+        set { damageAmount = value; }
+    }
+}
---
+++ Assets/Project/Scripts/ImageFillSetter.cs
@@ -0,0 +1,15 @@
+﻿using ScriptableObjectArchitecture;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageFillSetter : MonoBehaviour
+{
+    [SerializeField] private FloatReference variable = default(FloatReference);
+    [SerializeField] private FloatReference maxValue = default(FloatReference);
+    [SerializeField] private Image image = default(Image);
+
+    private void Update()
+    {
+        image.fillAmount = Mathf.Clamp01(variable.Value / maxValue.Value);
+    }
+}
