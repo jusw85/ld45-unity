9324c04632bbf51019252a095f0b0547198ea504
ziczach <czc85@hotmail.com>
2019-10-06 16:20:13 +0800

    Commit Tsx file

--- Assets/Project/Tilemaps/Environment_Sprite_Sheet_32.tsx
+++ Assets/Project/Tilemaps/Environment_Sprite_Sheet_32.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="Environment_Sprite_Sheet_64" tilewidth="32" tileheight="32" tilecount="512" columns="32">
- <image source="spritesheets/Environment_Sprite_Sheet_64.png" width="1024" height="512"/>
+<tileset version="1.2" tiledversion="1.2.4" name="Environment_Sprite_Sheet_64" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="spritesheets/Environment_Sprite_Sheet_64.png" width="1024" height="1024"/>
 </tileset>
