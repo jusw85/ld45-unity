253e6761c6e634612eec10ca318ca7cdaae7474a
Justin Wong <jusw85@hotmail.com>
2019-10-07 14:01:27 +0800

    Add trap damage

---
+++ Assets/Project/Scripts/ObjectSpawner.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject objectToSpawn;
+    [SerializeField] private float spawnDelay = 10.0f;
+
+    private void Start()
+    {
+        StartCoroutine(CoroutineUtils.Repeat(
+            () => { Instantiate(objectToSpawn, transform.position, transform.rotation); },
+            0, spawnDelay));
+    }
+}
