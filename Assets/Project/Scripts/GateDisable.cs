b881f235316f83f3cfe6170ed98b278c41c6b391
Justin Wong <jusw85@hotmail.com>
2019-10-07 23:35:48 +0800

    Add ending transition

--- Assets/Project/Scripts/GateDisable.cs
+++ Assets/Project/Scripts/GateDisable.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using ScriptableObjectArchitecture;
 using UnityEngine;
 
 public class GateDisable : MonoBehaviour
 {
     [SerializeField] private GameObject toDestroy;
     [SerializeField] private GameObject toEnable;
-
+    [SerializeField] private GameEvent pickup;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.transform.root.tag.Equals("Player"))
         {
+            pickup.Raise();
             Destroy(toDestroy);
             toEnable.SetActive(true);
             Destroy(gameObject);
