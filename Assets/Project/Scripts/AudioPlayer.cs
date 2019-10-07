30dd9b7032051249471dfb7b6b122939ff389c73
Justin Wong <jusw85@hotmail.com>
2019-10-07 17:26:39 +0800

    Add audio mixer

---
+++ Assets/Project/Scripts/AudioPlayer.cs
@@ -0,0 +1,30 @@
+﻿using MyBox;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioPlayer : MonoBehaviour
+{
+    [SerializeField] private AudioClip bgm;
+    [Range(0.001f, 1.0f)][SerializeField] private float bgmVolume = 1.0f;
+
+    private AudioSource source;
+    private AudioMixer mixer;
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+        mixer = source.outputAudioMixerGroup.audioMixer;
+        source.clip = bgm;
+        source.Play();
+    }
+
+    private void Start()
+    {
+        mixer.SetFloat("BGM Volume", VolumeToDecibel(bgmVolume));
+    }
+
+    private float VolumeToDecibel(float linearVolume)
+    {
+        return Mathf.Log10(Mathf.Clamp(bgmVolume, 0.001f, 1.0f)) * 20;
+    }
+}
