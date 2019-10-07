f53bded23c6a363f6d8e83034f050ee0a8499c2c
ziczach <czc85@hotmail.com>
2019-10-07 11:26:16 +0800

    Added Cloud Spawner. Added Waterfall
    Justin, need you to help to adjust the layer settings, the clouds that spawn cant be seen in the game view.

---
+++ Assets/Project/Scripts/CloudSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour
+{
+	
+	public GameObject objectToSpawn;
+	public float spawnDelay = 10.0f;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine ("Spawn");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	
+	IEnumerator Spawn () 
+	{
+		while (true)
+		{
+		
+		Instantiate (objectToSpawn, transform.position, transform.rotation);
+		yield return new WaitForSeconds (10f);
+		
+		}
+	}
+}
