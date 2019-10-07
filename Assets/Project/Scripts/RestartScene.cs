33b313293aa584009b7afdca14f323e4bcc1d785
Justin Wong <jusw85@hotmail.com>
2019-10-07 20:45:22 +0800

    Add restart scene

---
+++ Assets/Project/Scripts/RestartScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartScene : MonoBehaviour
+{
+    public void DoRestartScene()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+}
